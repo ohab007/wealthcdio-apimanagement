cd3103bd8d379026e2df67c47ad826b91ae5d3cd
Ansari <ohab-ali.ansari@capgemini.com>
2026-01-29 15:48:18 +0530

    my first commit to project

---
+++ src/main/java/com/cg/trafic_light/TraficLightApplication.java
@@ -0,0 +1,13 @@
+package com.cg.trafic_light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraficLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraficLightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/trafic_light/TraficLightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.trafic_light;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraficLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
